ec288a1a3caa4b109fe2fbae8830ad8c82de5d12
gabrielv121 <gabrielverna51@gmail.com>
2023-04-13 20:38:42 -0400

    updates

--- index.js
+++
@@ -1,5 +0,0 @@
-// Get the element with ID "my-element"
-const myElement = document.getElementById("my-element");
-
-// Update the text content of the element
-myElement.textContent = "Hello, world!";
